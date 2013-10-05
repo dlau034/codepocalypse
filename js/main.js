12b3700c56fc670cbe71105750f8e3f8cdcff5d8
gabriel <gabriel@CATCHs-MacBook-Air.local>
2013-10-05 17:12:15 -0400

    Functionality is in. Ready to add the design

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
-var words = ['console', 'function', 'bracket', 'log', 'procedure'];
-var score = 0;
+var words = ['console', 'function', 'bracket', 'log', 'procedure', 'tag', 'object', 'array', 'variable', 'recursive', 'prototype', 'attribute', 'src', 'href', 'border-radius', 'instance', 'inheritance'],
+	lives 		 = 5, // the initial number of lives
+	score 		 = 0, scoreMultiplier = 100,   // the current score 					// the score mutlipler
+	speedDefault = 1000, speedFactor  = 100,   // the default speed in millisiconds		// the multiplication factor related to the score	 	 
+	explosions 	 = ['wooble', 'pulse', 'swing', 'tada', 'flip', 'flipInX', 'flipOutX', 'fadeOutUp', 'fadeOutLeft', 'fadeOutUpBig', 'slideInDown', 'slideOutUp', 'slideOutRight', 'bounceIn', 'bounceInUp', 'bounceInDown', 'bounceOutUp', 'rotateInUpLeft', 'rotateInDownLeft', 'rotateInDownRight', 'rotateOut', 'rotateOutDownLeft', 'lightSpeedIn', 'lightSpeedOut', 'hinge', 'rollIn', 'rollOut']; // the explosion types
+	
+	
+	
+	
+var input = $('#text-field'); //the textarea
+
 
 // Populate the DOM with the words from the above array
 function populateWithWords(callback) {
 
-	for (var i in words) {		
-		$('.words').append('<li id="word-'+i+'">'+words[i]+'</li>');
+	for (var i in words.reverse()) {		
+		$('.words').append('<div id="word-'+i+'" class="animated">'+words[i]+'</div>');
 	}
 	
 	if (callback != undefined) {
@@ -23,16 +32,30 @@ function populateWithWords(callback) {
  * increase the score
  * and call the gravity for the next word
  */
-$('#text-field').keyup(function(a) {
-	if ($(this).val() == $('.words').find('li').first().html()) {				
-		$('.words').find('li').first().stop().remove();
+input.keyup(function(a) {
+	if ($(this).val() == $('.words').find('div').last().html()) {				
 		
-		$(this).val(''); //reset the field
+		var elm 		= $('.words').find('div').last(),
+			//explosion	= explosions[Math.floor(Math.random() * explosions.length)]; // choose a random explosion type
+			explosion	= 'bounceOutUp';
+			
+			
+		// stop the animation and add the explosion class		
+		elm.stop().addClass(explosion);
+		console.log('explosion type', explosion);
 		
-		score++; // increase the score		
-		$('.score').html(score); 
+		var explosionTimer = setTimeout(function(){
+			elm.remove(); // remove the word from the DOM
+
+			// call the gravity for the next word
+			gravity($('.words').find('div').last()); 
+			
+		}, 700); //the duration is equal to the css animation duration
+
+		score++; // increase the score
+		setScore(score);		
+		$(this).val('').focus(); //reset the field
 		
-		gravity($('.words').find('li').first()); // call the gravity for the next word
 	}
 });
 
@@ -45,24 +68,52 @@ $('#text-field').keyup(function(a) {
  * which means the score decreases by 1.
  */
 function gravity(elm) {
-		
-	elm.animate({
-		'top'	:  '100%',
-	}, 5000, function(){
-		
-		score--; // decrease the score
-		$('.score').html(score);		
-		
-		elm.remove(); 
-		gravity($('.words').find('li').first()); // call the gravity for the next word
-	})
 	
+	//speed = speedDefault - (score * speedFactor);
+	speed = speedDefault;
+	var left = (50 - Math.floor(Math.random()*100))+'%'; // the left angle running from -50% to +50%
+	
+	// log the speed and the angle for reference
+	console.log('speed: ', speed); 
+	console.log('left: ', left);
+	
+	elm.animate({
+		'top' :  $(window).height(),
+		'easing' : 	'linear',
+		'left'	 : 	left 
+	}, speed, function(){
+		
+		// Make sure the user still have enough lives to move on
+		if (lives > 0) {
+			lives--; // decrease the score
+			setLives(lives);
+			
+			elm.remove(); 
+			input.val('').focus(); //reset the field
+		
+			gravity($('.words').find('div').last()); // call the gravity for the next word	
+		} else { // if not GAME OVER
+			$('#game-over').removeClass('hidden').addClass('animated tada');
+		}
+	});
+	
+}
+
+function setScore(newScore) {
+	$('.score').html(newScore * scoreMultiplier);
+}
+
+function setLives(newLives) {
+	$('.lives').html(newLives);
 }
 
 
 
 /* Initiate the whole thing */
 populateWithWords(function(){
-	gravity($('.words').find('li').first());
+	setScore(score);
+	setLives(lives);
+	input.focus();
+	gravity($('.words').find('div').last());
 });
 
